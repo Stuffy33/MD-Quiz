2c95f72f05a4f2399e0c77aea6016982be0976d4
Stuffy33 <eric96an@gmail.com>
2022-04-18 21:42:18 +0000

    buttons light up green if correct red if not. new bug appeared

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,63 +1,68 @@
 //Question bank
 let questionBank = [
-     {
-         question: "What is actually electricity?",
-         option: ["A flow of water", "A flow of air", "A flow of electrons", "A flow of atoms"],
-         answer: "A flow of electrons"
-     },
-     {
-         question: "Which of the following disorders is the fear of being alone?",
-         option: [
-             "Agoraphobia",
-             "Aerophobia",
-             "Acrophobia", 
-             "Arachnophobia"
-         ],
-         answer: "Agoraphobia"
-     },
-     {
-         question: "What is the speed of sound?",
-         option: [
-             "120 km/h", 
-             "1,200 km/h", 
-             "400 km/h", 
-             "700 km/h"
-         ],
-         answer: "1,200 km/h"
-     },
-     {
-         question: "What is the main component of the sun?",
-         option: ["Liquid lava", "Gas", "Molten iron", "Rock"],
-         answer: "Gas"
-     },
-     {
-         question: "The property of a material which enables it to be drawn into wires with the application of a tensile force, is called?",
-         option: [
-             "Plasticity", 
-             "Elasticity", 
-             "Ductility", 
-             "Malleability"
-         ],
-         answer: "Ductility"
-     },
+    {
+    answer: "A flow of electrons",
+    option: [
+    "A flow of water",
+    "A flow of air",
+    "A flow of electrons",
+    "A flow of atoms"
+    ],
+    question: "What is actually electricity?"
+    },
+    {
+    answer: "Agoraphobia",
+    option: [
+    "Agoraphobia",
+    "Aerophobia",
+    "Acrophobia",
+    "Arachnophobia"
+    ],
+    question: "Which of the following disorders is the fear of being alone?"
+    },
+    {
+    answer: "1,200 km/h",
+    option: [
+    "120 km/h",
+    "1,200 km/h",
+    "400 km/h",
+    "700 km/h"
+    ],
+    question: "What is the speed of sound?"
+    },
+    {
+    answer: "Gas",
+    option: ["Liquid lava", "Gas", "Molten iron", "Rock"],
+    question: "What is the main component of the sun?"
+    },
+    {
+    answer: "Ductility",
+    option: [
+    "Plasticity",
+    "Elasticity",
+    "Ductility",
+    "Malleability"
+    ],
+    question: "The property of a material which enables it to be drawn into wires with the application of a tensile force, is called?"
+    }
 ]
 
-let question = document.getElementById("question");
 let quizContainer = document.getElementById("quiz-container");
 let scorecard = document.getElementById("scorecard");
+let question = document.getElementById("question");
+let nextBtn = document.querySelector(".next-btn");
 let option0 = document.getElementById("option0");
 let option1 = document.getElementById("option1");
 let option2 = document.getElementById("option2");
 let option3 = document.getElementById("option3");
-let nextBtn = document.querySelector(".next-btn");
 let option = document.querySelector(".option");
 let points = document.getElementById("score");
-let i = 0;
 let score = 0;
+let i = 0;
 
 //function to display questions
 function displayQuestion(){
-    for(let a = 0; a < option.length; a++){
+    for(a = 0; a < option.length; a++){
         option[a].style.background = "none";
     }
     question.innerHTML = questionBank[i].question;
@@ -67,7 +72,7 @@ function displayQuestion(){
     option3.innerHTML = questionBank[i].option[3];
     
     //Display xof5
-    xof5.innerHTML = "Question" + ' ' + (i + 1) + ' ' + 'of' + ' ' + questionBank.length;
+    xof5.innerHTML = "Question" + " " + (i + 1) + " " + "of" + " " + questionBank.length;
 }
 
 //function to calculate scores
@@ -79,7 +84,7 @@ function calcScore(event){
     else{
         document.getElementById(event.id).style.background= "red";
     }
-    setTimeout(nextQuestion, 300);
+    setTimeout(nextQuestion, 400);
     
 }
 
@@ -90,7 +95,7 @@ function nextQuestion(){
         displayQuestion();
     }
     else{
-        points.innerHTML= score + "/" + questionBank.length;
+        points.innerHTML = score + "/" + questionBank.length;
         quizContainer.style.display = "none";
         scoreboard.style.display= "block"
     }
@@ -99,8 +104,8 @@ function nextQuestion(){
 //click event to next-btn
 nextBtn.addEventListener("click", nextQuestion);
 
-//Back to Quiz
-function backToQuiz(){
+//Redo Quiz
+function redoQuiz(){
     location.reload();
 }
 
@@ -110,7 +115,8 @@ function checkAnswer(){
     let answers = document.getElementById("answers");
     answerContainer.style.display = "block";
     scoreboard.style.display = "none";
-    for (let a = 0; a < questionBank.length; a++){
+    
+    for (a = 0; a < questionBank.length; a++){
         let list = document.createElement("li");
         list.innerHTML = questionBank[a].answer;
         answers.appendChild(list);
